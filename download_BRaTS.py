59351f2886ceaeb4c190422b98b368ba01a44c42
Nontakorn Bunluesriruang <bunluesriruangnontakorn@gmail.com>
2026-04-12 06:37:49 +0700

    save changes

--- download_BRaTS.py
+++ download_BRaTS.py
@@ -1,7 +1,14 @@
 import kagglehub
 
+'''
+Note!! Change default location of kagglehub download before downloading. 
+I have no idea how but beware if computer storage is almost full.
+
+'''
+
+
 # Download latest version
-path = kagglehub.dataset_download("awsaf49/brats2020-training-data") #BRATS 2020 Dataser
+path = kagglehub.dataset_download("awsaf49/brats2020-training-data", path='') #BRATS 2020 Dataser
 path = kagglehub.dataset_download("nguyenthanhkhanh/brats2024-small-dataset") #BRATS 2024 Data
 
 print('Dataset downloaded to:', path)
